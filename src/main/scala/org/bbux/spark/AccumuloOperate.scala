d30ad662a4ca1747096bbfdb14b722383a32c1fb
Brian Buxton <bbuxton@proteuseng.com>
2014-05-09 08:45:52 -0400

    modified accumulo test to take args from command line

--- src/main/scala/org/bbux/spark/AccumuloOperate.scala
+++ src/main/scala/org/bbux/spark/AccumuloOperate.scala
@@ -1,14 +1,16 @@
 package org.bbux.spark;
 
 import org.apache.spark._
+import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.NewHadoopRDD
 
 import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.io.Text
 
 import org.apache.accumulo.core.client.mapreduce.InputFormatBase
 import org.apache.accumulo.core.client.mapreduce.AccumuloInputFormat
 
-object AccumuloTest {
+object AccumuloOperate {
   def main(args: Array[String]) {
   
     if (args.length < 6) {
@@ -34,8 +36,16 @@ object AccumuloTest {
       classOf[org.apache.accumulo.core.data.Key],
       classOf[org.apache.accumulo.core.data.Value])
 
-    accumuloRDD.count()
+    val famValRDD  = accumuloRDD.map { tuple =>
+       val key = tuple._1
+       val value = tuple._2
+       val outKey = key.getColumnFamily().toString() + new String(value.get())
+       (outKey, 1)
+    }.reduceByKey(_+_, 1)
 
+    famValRDD.foreach(println)
+    //println("count is: " + cnt)
+    
     System.exit(0)
   }
 }
---
+++ test-op.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+stty -echo
+read -p "Password: " passw; echo
+stty echo
+
+java -cp target/spark-accumulo-ingest-0.1.0-SNAPSHOT-shaded.jar org.bbux.spark.AccumuloOperate \
+  local \
+  root \
+  $passw \
+  grades \
+  pixydust \
+  localhost
